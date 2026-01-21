4fad1f04094ab3b575f865b95f9300739d01b7e5
clovisTOTOT <dongmotiomomathias33@gmail.com>
2026-01-21 03:50:32 -0500

    Premier depot du projet de l'hopital Marie-Wyss

---
+++ apps/views.py
@@ -0,0 +1,62 @@
+
+from  django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def home(request):
+
+        return  render(request, 'accueille.html')
+
+
+def services(request):
+    return render(request, 'services.html')  
+
+def actualite(request):
+    return render(request, 'actualite.html')
+
+def apropos(request):
+    return render(request, 'apropos.html')
+
+def galerie_image(request):
+    return render(request, 'galerie-image.html') 
+
+def teleconsultation(request):
+    return render(request, 'teleconsultation.html') 
+
+def contacts(request):
+    return render(request, 'contacts.html') 
+
+def header(request):
+    return render(request, 'header.html')
+
+def footer(request):
+    return render(request, 'footer.html')       
+
+def docteur1_view(request):
+    return render(request, 'Docteur1.html')
+
+def docteur2_view(request):
+    return render(request, 'Docteur2.html')
+
+def docteur3_view(request):
+    return render(request, 'Docteur3.html')
+
+def docteur4_view(request):
+    return render(request, 'Docteur4.html')
+
+def book_appointment(request):
+    if request.method == 'POST':
+        # Traitez les données du formulaire ici
+        pass
+    return render(request, 'appointment.html')  # Remplacez par le bon template
+
+def privacy_policy_view(request):
+    return render(request, 'privacy_policy.html')  # Remplacez par le bon template   
+
+
+def notreequipe_view(request):
+    return render(request, 'notreequipe.html')
+
+
+
---
+++ educ_proc/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for educ_proc project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from apps import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Ajoutez cette ligne pour la racine
+    path('admin/', admin.site.urls),
+    # Direct routes for docteurs (so links like /Docteur2/ work)
+    path('Docteur1/', views.docteur1_view, name='docteur1'),
+    path('Docteur2/', views.docteur2_view, name='docteur2'),
+    path('Docteur3/', views.docteur3_view, name='docteur3'),
+    path('Docteur4/', views.docteur4_view, name='docteur4'),
+    path('notre-equipe/', views.notreequipe_view, name='notre_equipe'),
+    path('apps/', include('apps.urls')),
+    path('__reload__/', include('django_browser_reload.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
